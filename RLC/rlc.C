c437cb552cc621156b1a5cce06b2b3167d940690
Alberto Perro <albywarwick@gmail.com>
2018-04-13 15:22:13 +0200

    grafici e ultime correzioni

--- RLC/rlc.C
+++ RLC/rlc.C
@@ -43,12 +43,13 @@ void BANDpass(){
     sT[i]=sf[i]*T[i]/freq[i];
     Av[i]=Vo[i]/Vi[i];
     sAv[i] = Av[i]*sqrt(pow(sVo[i]/Vo[i],2)+pow(sVi[i]/Vi[i],2));
-    phase[i] = 360*freq[i]*abs(skew[i])*1e-6;
+    //phase[i] = 360*freq[i]*abs(skew[i])*1e-6;
     skew[i]<-1 ? sPhase[i] = phase[i]*sqrt(pow(sf[i]/freq[i],2)+pow(1/skew[i],2)):sPhase[i] = phase[i]*sqrt(pow(sf[i]/freq[i],2)+pow(0.01/skew[i],2));
     printf("f(Hz): (%.2f±%.2f) \t Vi(V): (%.1f±%.1f) \t Vo(V): (%.2f±%.2f) \t Av(unitario): (%.f±%.4f) \n",freq[i],sf[i],Vi[i],sVi[i],Vo[i],sVo[i],Av[i],sAv[i]);
     //skew[i]<0 ? skew[i]=T[i]+skew[i] : skew[i]=skew[i]; // converto le fasi in positivo
     phase[i] = 360*freq[i]*skew[i]*1e-6;
     phase[i] > 90 ? phase[i]-=360 : 1;
+    phase[i]=-phase[i];
     sPhase[i]=5;
     //skew[i]>1000 ? sPhase[i] = phase[i]*sqrt(pow(sf[i]/freq[i],2)+pow(5000/skew[i],2)):sPhase[i] = phase[i]*sqrt(pow(sf[i]/freq[i],2)+pow(1/skew[i],2));
     //printf("f(Hz): (%.2f±%.2f) \t Vi(V): (%.1f±%.1f) \t Vo(V): (%.2f±%.2f) \t Av(unitario): (%.f±%.4f) \n",freq[i],sf[i],Vi[i],sVi[i],Vo[i],sVo[i],AdB[i],sAdB[i]);
@@ -71,7 +72,9 @@ void BANDpass(){
   cout << "\n\n --- Fit Guadagno \n" <<endl;
   TF1 *lfit = new TF1("fit","x*[0]*[0]/sqrt(x*x*pow([0],4)*(1+[1])*(1+[1])+[2]*[2]*(x*x-[0]*[0])*(x*x-[0]*[0]))",30,5000);
   lfit->SetParameter(0,500);
-  lfit->SetLineColor(4);
+  lfit->SetParameter(1,1e-1);
+  lfit->SetParameter(2,300);
+  lfit->SetLineColor(14);
   gav->Fit(lfit,"R+");
   cout << "Chi^2:" << lfit->GetChisquare() << ", number of DoF: " << lfit->GetNDF() << " (Probability: " << lfit->GetProb() << ")." << endl;
   cout << "--------------------------------------------------------------------------------------------------------" << endl;
@@ -87,18 +90,35 @@ void BANDpass(){
   gph->SetMarkerSize(0.6);
   gph->SetMarkerStyle(21);
   // Facile, titolo del grafico
-  gph->SetTitle("Phase(f)");
+  gph->SetTitle("Fase(f)");
   // Titoli degli assi
   gph->GetXaxis()->SetTitle("freq [Hz]");
-  gph->GetYaxis()->SetTitle("phase [deg]");
+  gph->GetYaxis()->SetTitle("Fase [deg]");
   gph->Draw("AP");
   cout << "\n\n --- Fit Fase \n" <<endl;
   TF1 *afit = new TF1("afit","(180/3.1415)*atan(([2]/(1+[1]))*([0]*[0]-x*x)/([0]*[0]*x))",0,5000);
   afit->SetParameter(0,500);
-  afit->SetLineColor(4);
+  afit->SetParameter(1,1e-1);
+  afit->SetParameter(2,300);
+  afit->SetLineColor(14);
   gph->Fit(afit,"R+");
   cout << "Chi^2:" << afit->GetChisquare() << ", number of DoF: " << afit->GetNDF() << " (Probability: " << afit->GetProb() << ")." << endl;
   cout << "--------------------------------------------------------------------------------------------------------" << endl;
+  TCanvas *cV = new TCanvas("Vh","Vi(f)",200,10,600,400);
+  cV->SetFillColor(0);
+  cV->cd();
+  cV->SetLogx();
+  TGraphErrors *gV = new TGraphErrors(n,freq,Vi,sf,sVi);
+  gV->SetMarkerSize(0.6);
+  gV->SetMarkerStyle(21);
+  // Facile, titolo del grafico
+  gV->SetTitle("V_{in}(f)");
+  gV->SetLineColor(14);
+  gV->SetMinimum(0);
+  // Titoli degli assi
+  gV->GetXaxis()->SetTitle("freq [Hz]");
+  gV->GetYaxis()->SetTitle("V_{in} [V]");
+  gV->Draw("LAP");
 }
 void rlc(){
 BANDpass();
